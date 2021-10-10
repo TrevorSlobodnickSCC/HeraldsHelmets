2713cd087c5b520cf018c0f69ab19ca3e8d5f647
TrevorSlobodnickSCC <W0740591@myscc.ca>
2021-10-10 13:05:30 -0400

    Init Commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HeraldsHelmets.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
